0814712a86a0225f78522f9628f1cada9fe9957f
Steven Eiselt <seiselts@gmail.com>
2022-05-18 10:18:19 -0700

    updated readme and title

--- src/Title.js
+++ src/Title.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Title = () => {
-  return <div>See a Color and its Hex Code!</div>;
+  return (
+    <div>
+      <h1>See a Color and its Hex Code!</h1>
+      <p>
+        Type a color in the input field and the square will change that color!
+      </p>
+    </div>
+  );
 };
 
 export default Title;
